8ffa3af92e7255d3009b51cda6e17285668d3153
Reimar Rosas <reimarrosas0208@gmail.com>
2022-01-03 18:59:20 -0500

    Added default background and color, letter spacing, and custom properties in the global css
    Implemented and styled the Header component that will contain my name
    and the navigation bar of the site

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Navigation from "./Navigation";
+import styles from "../styles/Header.module.css";
+
+const Header: React.FC = () => {
+  return (
+    <div className={styles.headerContainer}>
+      <header className={styles.header} id="header">
+        <a href="#header">
+          <h1 className={styles.headerTitle}>Reimar Rosas</h1>
+        </a>
+        <Navigation />
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Navigation.tsx
@@ -0,0 +1,21 @@
+import styles from "../styles/Navigation.module.css";
+
+const Navigation: React.FC = () => {
+  return (
+    <nav>
+      <ul className={styles.navlist}>
+        <a className={styles.navlink} href="https://blog.reimarrosas.me">
+          <li>Blog</li>
+        </a>
+        <a className={styles.navlink} href="#projects">
+          <li>Projects</li>
+        </a>
+        <a className={styles.navlink} href="#contact">
+          <li>Contact</li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
