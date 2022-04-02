f4d046084d158d2b34ae8dfe2fdb56a663c39fe0
Reimar Rosas <reimarrosas0208@gmail.com>
2022-04-01 21:54:04 -0400

    Fixed the project link to use next/link instead of anchor element

--- components/Header/Navigation.tsx
+++ components/Header/Navigation.tsx
@@ -1,16 +1,19 @@
-import styles from "../../styles/Header/Navigation.module.css";
+import Link from 'next/link';
+import styles from '../../styles/Header/Navigation.module.css';
 
 const Navigation: React.FC = () => {
   return (
     <nav>
       <ul className={styles.navlist}>
-        <a className={styles.navlink} href="https://blog.reimarrosas.me">
+        <a className={styles.navlink} href='https://blog.reimarrosas.me'>
           <li>Blog</li>
         </a>
-        <a className={styles.navlink} href="/projects">
-          <li>Projects</li>
-        </a>
-        <a className={styles.navlink} href="#contact">
+        <Link href='/projects'>
+          <a className={styles.navlink}>
+            <li>Projects</li>
+          </a>
+        </Link>
+        <a className={styles.navlink} href='#contact'>
           <li>Contact</li>
         </a>
       </ul>
